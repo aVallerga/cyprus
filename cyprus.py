1e88a24b0d9bb43b7d84a160e431d7946d6ba4c8
Blaine Pace <blainepace@gmail.com>
2010-12-30 01:40:48 -0800

    Added some sanity checks.

--- cyprus.py
+++ cyprus.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python
 
-import imdb
 import sys
+
+try:
+	import imdb
+except ImportError:
+	print "Please install the IMDbPY package. python-imdbpy in Ubuntu."
+	sys.exit(1)
+
+# check for one argument
+if len(sys.argv) != 2:
+	print "Only one argument is required:"
+	print "   %s 'movie title'" % sys.argv[0]
+	sys.exit(2)
 
 ia = imdb.IMDb()
 
-s_result = ia.search_movie(sys.argv[1])
+searchterm = sys.argv[1]
+
+try:
+	# try searching for list of results
+	s_result = ia.search_movie(searchterm)
+except imdb.IMDbError, e:
+	print "You probably have no internet connection. Complete error report: "
+	print e
+	sys.exit(3)
+
+if not s_result:
+	print "No matches for %s, sorry." % searchterm 
+	sys.exit(0)
 
 print "Search results: "
 i = 1
