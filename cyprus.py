a17a359c685a7cc10efa99723a82c1fb847ee8b5
Blaine Pace <blainepace@gmail.com>
2010-12-30 01:44:54 -0800

    Removed long listing of results.

--- cyprus.py
+++ cyprus.py
@@ -30,13 +30,6 @@ if not s_result:
 	print "No matches for %s, sorry." % searchterm 
 	sys.exit(0)
 
-print "Search results: "
-i = 1
-for item in s_result:
-		print i,
-		print item['long imdb canonical title'], item.movieID
-		i = i + 1
-		
 first_result = s_result[0]
 ia.update(first_result)
 
