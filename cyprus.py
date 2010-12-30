aa38039b57ed169a7e041cfc3b1c974b759f0d3e
Blaine Pace <blainepace@gmail.com>
2010-12-30 01:34:38 -0800

    Added some formatting.

--- cyprus.py
+++ cyprus.py
@@ -7,5 +7,15 @@ ia = imdb.IMDb()
 
 s_result = ia.search_movie(sys.argv[1])
 
+print "Search results: "
+i = 1
 for item in s_result:
+		print i,
 		print item['long imdb canonical title'], item.movieID
+		i = i + 1
+		
+first_result = s_result[0]
+ia.update(first_result)
+
+print "\nBest match: "
+print first_result.summary()
